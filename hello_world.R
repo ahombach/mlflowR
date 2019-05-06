5c64de8af241acf3bfda5473b84f04cc038a8e14
ahombach <andrew.hombach@databricks.com>
2019-05-06 16:20:46 -0400

    added repo

--- hello_world.R
+++ hello_world.R
@@ -1,7 +1,7 @@
 print("hello world")
 print("what up yo")
 
-install.packages("packrat")
+install.packages("packrat", repos = "http://cran.us.r-project.org")
 
 packrat::unbundle("/tmp/r-prod/packrat/bundles/mlflowR-2019-05-06.tar.gz")
 
