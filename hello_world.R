8e477fa6b1c61d93dff058c210e310d60917bcea
ahombach <andrew.hombach@databricks.com>
2019-05-06 16:18:23 -0400

    added unpack

--- hello_world.R
+++ hello_world.R
@@ -1,4 +1,8 @@
 print("hello world")
 print("what up yo")
 
+install.packages(packrat)
+
+packrat::unbundle("/tmp/r-prod/packrat/bundles/mlflowR-2019-05-06.tar.gz")
+
 print(packageVersion("callr"))
