6da68ceb4d8097905d70e49651dcd32ebd1335b4
ahombach <andrew.hombach@databricks.com>
2019-05-06 17:13:05 -0400

    added package version tests

--- hello_world.R
+++ hello_world.R
@@ -1,6 +1,6 @@
 print("hello world")
 print("what up yo")
 
-packrat::unbundle("/tmp/r-prod/packrat/bundles/mlflowR-2019-05-06.tar.gz")
-
 print(packageVersion("callr"))
+print(packageVersion("devtools"))
+print(packageVersion('askpass'))
